9f6b54c6b681f611b0a44fef752fad3c30c121bd
Oleh_K <oleh.usa2017@gmail.com>
2023-09-02 23:27:54 -0400

    lab02

--- lab-02-di/src/main/java/com/cydeo/model/Product.java
+++ lab-02-di/src/main/java/com/cydeo/model/Product.java
@@ -1,14 +1,28 @@
 package com.cydeo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 @Getter
 @Setter
+@Component
 public class Product {
     private String name;
     private int quantity;
     private int remainingQuantity;
     private BigDecimal price;
+
+
+    public Product(String name, int quantity, int remainingQuantity, BigDecimal price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.remainingQuantity = remainingQuantity;
+        this.price = price;
+    }
+
+    public Product() {
+    }
 }
